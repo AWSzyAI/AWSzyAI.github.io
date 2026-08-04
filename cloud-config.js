0569bea43154b2f83ea2e9ca8bb91bf321b0804f
Ziyan Shi <szy@nnu.edu.cn>
2026-08-04 22:03:33 +0800

    Configure Supabase cloud sync

--- cloud-config.js
+++ cloud-config.js
@@ -1,5 +1,5 @@
 window.SZY_CLOUD_CONFIG = Object.freeze({
-    supabaseUrl: '',
-    supabasePublishableKey: '',
+    supabaseUrl: 'https://qpiitrxuhcbxrdpcmmyj.supabase.co',
+    supabasePublishableKey: 'sb_publishable_Myfr2o6Z9nrOI0orhPF8ig_DRP4lLM1',
     ownerGithubLogin: 'AWSzyAI'
 });
